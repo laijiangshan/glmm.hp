7540ea213cb77f4728732dccfde2d1cf901c79fc
RichardJTelford <Richard.Telford@uib.no>
2024-06-24 12:13:10 +0200

    use styler to clean code style

--- R/creatbin.r
+++ R/creatbin.r
@@ -1,4 +1,4 @@
-#'Internal function for glmm.hp() to create diagonal matrix
+#' Internal function for glmm.hp() to create diagonal matrix
 #' @param  col Imput number.
 #' @param  binmatrix Imput empty matrix.
 
@@ -6,30 +6,30 @@
 #' @return \item{a matix}{A diagonal matrix}
 #' @keywords internal
 creatbin <-
-function(col, binmatrix) {
-row<-1
-val<-col
-	while (val!=0){              
-	if (odd(val)) {
-		binmatrix[row,col]=1 
-	}
-	val<-as.integer(val/2)
-	row<-row+1
-}
-##Return matrix
-return(binmatrix)
-}
+  function(col, binmatrix) {
+    row <- 1
+    val <- col
+    while (val != 0) {
+      if (odd(val)) {
+        binmatrix[row, col] <- 1
+      }
+      val <- as.integer(val / 2)
+      row <- row + 1
+    }
+    ## Return matrix
+    return(binmatrix)
+  }
 
 
-#'Internal function for glmm.hp() to determine whether the odd number
+#' Internal function for glmm.hp() to determine whether the odd number
 #' @param  val Imput number.
 #' @return a logical value
 #' @return \item{Logical value}{TRUE or FALSE}
 #' @keywords internal
-odd <- function (val) 
-{
-    if (val%%2 == 0) 
-     return(FALSE)
-	 else
+odd <- function(val) {
+  if (val %% 2 == 0) {
+    return(FALSE)
+  } else {
     return(TRUE)
+  }
 }
